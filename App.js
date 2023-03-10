be236746c5c59c08191018ce08dd2bc11608125e
Mercy Mugambi <48310207+mercymugambi@users.noreply.github.com>
2023-03-10 11:54:19 +0300

    Add form validation javascript

--- App.js
+++ App.js
@@ -232,15 +232,15 @@ headPopCloseBtn.addEventListener('click', () => {
 
 // Form Validation
 
-// const errorMsg = document.getElementById('error-msg');
-// const email = document.getElementById('email');
-// const form = document.getElementById('form');
+const errorMsg = document.getElementById('error-msg');
+const email = document.getElementById('email');
+const form = document.getElementById('form');
 
-// form.addEventListener('submit', (e) => {
-//   if (email.value !== email.value.toLowerCase()) {
-//     e.preventDefault();
-//     errorMsg.style.display = 'flex';
-//   } else {
-//     errorMsg.style.display = 'none';
-//   }
-// });
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
+    e.preventDefault();
+    errorMsg.style.display = 'flex';
+  } else {
+    errorMsg.style.display = 'none';
+  }
+});
